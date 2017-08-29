ce157418aca3eb30f7003b5196c4ef2f6a52f0d8
mcmonkey4eva <agoodwin@freneticllc.com>
2017-08-28 20:58:12 -0700

    Inject vox OR fge license: sample of how to differentiate lazily

--- LicenseInjector/Program.cs
+++ LicenseInjector/Program.cs
@@ -8,17 +8,36 @@ namespace LicenseInjector
 {
     class Program
     {
-        const string LICENSE = "//\r\n"
-                             + "// This file is part of the game Voxalia, created by FreneticXYZ.\r\n"
-                             + "// This code is Copyright (C) 2016 FreneticXYZ under the terms of the MIT license.\r\n"
-                             + "// See README.md or LICENSE.txt for contents of the MIT license.\r\n"
-                             + "// If these are not available, see https://opensource.org/licenses/MIT\r\n"
+        const string VOX_LICENSE = "//\r\n"
+                             + "// This file is part of the game Voxalia, created by Frenetic LLC.\r\n"
+                             + "// This code is Copyright (C) 2016-2017 Frenetic LLC under the terms of a strict license.\r\n"
+                             + "// See README.md or LICENSE.txt in the source root for the contents of the license.\r\n"
+                             + "// If neither of these are available, assume that neither you nor anyone other than the copyright holder\r\n"
+                             + "// hold any right or permission to use this software until such time as the official license is identified.\r\n"
                              + "//\r\n"
                              + "\r\n";
 
-        static void Main(string[] args)
+        const string FGE_LICENSE = "//\r\n"
+                             + "// This file is created by Frenetic LLC.\r\n"
+                             + "// This code is Copyright (C) 2017 Frenetic LLC under the terms of a strict license.\r\n"
+                             + "// See README.md or LICENSE.txt in the source root for the contents of the license.\r\n"
+                             + "// If neither of these are available, assume that neither you nor anyone other than the copyright holder\r\n"
+                             + "// hold any right or permission to use this software until such time as the official license is identified.\r\n"
+                             + "//\r\n"
+                             + "\r\n";
+
+        const string FGE2_LICENSE = "//\r\n"
+                             + "// This file is created by Frenetic LLC.\r\n"
+                             + "// This code is Copyright (C) 2016-2017 Frenetic LLC under the terms of a strict license.\r\n"
+                             + "// See README.md or LICENSE.txt in the source root for the contents of the license.\r\n"
+                             + "// If neither of these are available, assume that neither you nor anyone other than the copyright holder\r\n"
+                             + "// hold any right or permission to use this software until such time as the official license is identified.\r\n"
+                             + "//\r\n"
+                             + "\r\n";
+
+        static void Apply(string[] f)
         {
-            foreach (string file in Directory.GetFiles("./", "*.cs", SearchOption.AllDirectories))
+            foreach (string file in f)
             {
                 string t = File.ReadAllText(file);
                 while (t.StartsWith("//") || t.StartsWith("\n") || t.StartsWith("\r\n"))
@@ -26,9 +45,27 @@ namespace LicenseInjector
                     int newline = t.IndexOf('\n');
                     t = t.Substring(newline + 1);
                 }
-                t = LICENSE + t;
+                if (file.Contains("FreneticGameEngine") && (file.Contains("FreneticScript") || file.Contains("FreneticDataSyntax")))
+                {
+                    t = FGE_LICENSE + t;
+                }
+                else if (file.Contains("FreneticGameEngine"))
+                {
+                    t = FGE_LICENSE + t;
+                }
+                else
+                {
+                    t = VOX_LICENSE + t;
+                }
                 File.WriteAllText(file, t);
             }
+        }
+
+        static void Main(string[] args)
+        {
+            Apply(Directory.GetFiles("./", "*.cs", SearchOption.AllDirectories));
+            Apply(Directory.GetFiles("./", "*.fs", SearchOption.AllDirectories));
+            Apply(Directory.GetFiles("./", "*.vs", SearchOption.AllDirectories));
         }
     }
 }
