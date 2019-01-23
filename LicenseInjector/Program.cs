68332409be92e5a1bae3e1d63f6e7bfde48ab2ec
mcmonkey <agoodwin@freneticllc.com>
2019-01-22 22:06:38 -0800

    modern vox and subrepo license setup

--- LicenseInjector/Program.cs
+++ LicenseInjector/Program.cs
@@ -10,28 +10,33 @@ namespace LicenseInjector
     {
         const string VOX_LICENSE = "//\r\n"
                              + "// This file is part of the game Voxalia, created by Frenetic LLC.\r\n"
-                             + "// This code is Copyright (C) 2016-2017 Frenetic LLC under the terms of a strict license.\r\n"
-                             + "// See README.md or LICENSE.txt in the source root for the contents of the license.\r\n"
+                             + "// This code is Copyright (C) Frenetic LLC under the terms of a strict license.\r\n"
+                             + "// See README.md or LICENSE.txt in the Voxalia source root for the contents of the license.\r\n"
                              + "// If neither of these are available, assume that neither you nor anyone other than the copyright holder\r\n"
                              + "// hold any right or permission to use this software until such time as the official license is identified.\r\n"
                              + "//\r\n"
                              + "\r\n";
 
         const string FGE_LICENSE = "//\r\n"
-                             + "// This file is created by Frenetic LLC.\r\n"
-                             + "// This code is Copyright (C) 2017 Frenetic LLC under the terms of a strict license.\r\n"
-                             + "// See README.md or LICENSE.txt in the source root for the contents of the license.\r\n"
+                             + "// This file is part of the Frenetic Game Engine, created by Frenetic LLC.\r\n"
+                             + "// This code is Copyright (C) Frenetic LLC under the terms of a strict license.\r\n"
+                             + "// See README.md or LICENSE.txt in the FreneticGameEngine source root for the contents of the license.\r\n"
                              + "// If neither of these are available, assume that neither you nor anyone other than the copyright holder\r\n"
                              + "// hold any right or permission to use this software until such time as the official license is identified.\r\n"
                              + "//\r\n"
                              + "\r\n";
 
-        const string FGE2_LICENSE = "//\r\n"
-                             + "// This file is created by Frenetic LLC.\r\n"
-                             + "// This code is Copyright (C) 2016-2017 Frenetic LLC under the terms of a strict license.\r\n"
-                             + "// See README.md or LICENSE.txt in the source root for the contents of the license.\r\n"
-                             + "// If neither of these are available, assume that neither you nor anyone other than the copyright holder\r\n"
-                             + "// hold any right or permission to use this software until such time as the official license is identified.\r\n"
+        const string FS_MIT_LICENSE = "//\r\n"
+                             + "// This file is part of FreneticScript, created by Frenetic LLC.\r\n"
+                             + "// This code is Copyright (C) Frenetic LLC under the terms of the MIT license.\r\n"
+                             + "// See README.md or LICENSE.txt in the FreneticScript source root for the contents of the license.\r\n"
+                             + "//\r\n"
+                             + "\r\n";
+
+        const string FRENUTIL_MIT_LICENSE = "//\r\n"
+                             + "// This file is part of Frenetic Utilities, created by Frenetic LLC.\r\n"
+                             + "// This code is Copyright (C) Frenetic LLC under the terms of the MIT license.\r\n"
+                             + "// See README.md or LICENSE.txt in the FreneticUtilities source root for the contents of the license.\r\n"
                              + "//\r\n"
                              + "\r\n";
 
@@ -45,9 +50,13 @@ namespace LicenseInjector
                     int newline = t.IndexOf('\n');
                     t = t.Substring(newline + 1);
                 }
-                if (file.Contains("FreneticGameEngine") && (file.Contains("FreneticScript") || file.Contains("FreneticDataSyntax")))
+                if (file.Contains("FreneticGameEngine") && file.Contains("FreneticScript") && file.Contains("FreneticUtilities"))
                 {
-                    t = FGE_LICENSE + t;
+                    t = FRENUTIL_MIT_LICENSE + t;
+                }
+                else if (file.Contains("FreneticGameEngine") && file.Contains("FreneticScript"))
+                {
+                    t = FS_MIT_LICENSE + t;
                 }
                 else if (file.Contains("FreneticGameEngine"))
                 {
